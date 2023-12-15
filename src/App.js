4febc3767169cf2e5a2a66680fa5c56180ba9aeb
oahidalgo <ohgovilla@gmail.com>
2023-12-15 11:26:50 -0600

    ADD - Tip Calculator Exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [pct1, setPct1] = useState(0);
+  const [pct2, setPct2] = useState(0);
+  const tip = bill * ((pct1 + pct2) / 2 / 100);
+
+  function handleReset() {
+    setBill(0);
+    setPct1(0);
+    setPct2(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Field title="How much was the bill?">
+        <input
+          type="number"
+          value={bill}
+          onChange={(e) => setBill(+e.target.value)}
+        />
+      </Field>
+      <Field title="How did you like the service?">
+        <Values pct={pct1} onSelectPct={setPct1} />
+      </Field>
+      <Field title="How did your friend like the service?">
+        <Values pct={pct2} onSelectPct={setPct2} />
+      </Field>
+      <Output bill={bill} tip={tip} />
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
+}
+
+function Field({ title, children }) {
+  return (
+    <div>
+      <label>{title}</label>
+      {children}
+    </div>
+  );
+}
+
+function Values({ pct, onSelectPct }) {
+  return (
+    <select value={pct} onChange={(e) => onSelectPct(Number(e.target.value))}>
+      <option value="0">Dissatisfied (0%)</option>
+      <option value="5">It was ok! (5%)</option>
+      <option value="10">It was good (10%)</option>
+      <option value="20">Absolutely Amazing! (20%)</option>
+    </select>
+  );
+}
+
+function Output({ bill, tip }) {
+  return <h3>{`You pay $${bill + tip} ($${bill} + $${tip} tip)`}</h3>;
 }
 
 export default App;
